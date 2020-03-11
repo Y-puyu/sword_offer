16656e3d04db717c31c70d35d4fd7176d3e7e638
YangLi <345462072@qq.com>
2020-03-11 18:04:23 +0800

    Interview Question 7

---
+++ 7. Rebuilding the binary tree.cpp
@@ -0,0 +1,34 @@
+// 方法一：暴搜
+// 执行用时 :20 ms, 在所有 C++ 提交中击败了79.51%的用户
+// 内存消耗 :14.5 MB, 在所有 C++ 提交中击败了100.00%的用户
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        return buildTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
+    }
+
+    TreeNode *buildTree(vector<int> &preorder, int pLeft, int pRight, vector<int> &inorder, int iLeft, int iRight) {
+        if (pLeft > pRight || iLeft > iRight) 
+            return NULL;
+        int i = 0;
+        for (i = iLeft; i <= iRight; ++i) {
+            if (preorder[pLeft] == inorder[i]) 
+                break;
+        }
+        TreeNode *cur = new TreeNode(preorder[pLeft]);
+        cur->left = buildTree(preorder, pLeft + 1, pLeft + i - iLeft, inorder, iLeft, i - 1);
+        cur->right = buildTree(preorder, pLeft + i - iLeft + 1, pRight, inorder, i + 1, iRight);
+        return cur;
+    }
+};
+
